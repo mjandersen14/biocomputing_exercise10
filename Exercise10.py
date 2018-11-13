cc409e3d9da029cd189c0c4f1872b640a725c45c
Marissa Andersen <marissaandersen@marissas-air.dhcp.nd.edu>
2018-11-13 13:07:00 -0500

    Just need to double check it all works

--- Exercise10.py
+++ Exercise10.py
@@ -44,41 +44,46 @@ print(fit.x)
 #################### QUESTION SCRIPT #########################
 import numpy
 import pandas
-from scipy.optimize import minimize #what does this mean? 
-from scipy.stats import norm #lol what 
+from scipy.optimize import minimize
+from scipy.stats import norm
 from plotnine import *
 
 data=pandas.read_csv('data.txt', sep=",", header=0)
 print (data)
-#is it going to hurt me later on that I'm using a dataframe and not a numpy array? 
+ 
 ggplot(data,aes(x='x',y='y'))+geom_point()+theme_classic()
 
-def nllike(p,xaxis):
-#changed obs to xaxis becuase i feel like it makes more sense but does it? what is it? 
+def nllike(p,obs):
     B0=p[0]
     B1=p[1]
     sigma=p[2]
-    expected=B0+B1*obs.x #hypothesis 
-    nll=-1*norm(expected,sigma).logpdf(obs.y).sum() #null model 
-    #can i just add in the quadratic here? 
+    expected=B0+B1*obs.x 
+    nll=-1*norm(expected,sigma).logpdf(obs.y).sum() 
     return nll
-# is this just the same for any linear model? what would change between models? 
-# I don't get what any of the lines mean or represent here.. 
+
+initialGuess=numpy.array([1,1,1])
+fit=minimize(nllike,initialGuess,method="Nelder-Mead",options={'disp': True},args=data)
+
     
-y=m*x+b
+def quad(p,obs):
+    B0=p[0]
+    B1=p[1]
+    B2=p[2]
+    sigma=p[3]
+    expected=B0+B1*obs.x+B2*((obs.x)**2)  
+    nll=-1*norm(expected,sigma).logpdf(obs.y).sum()
+    return nll
 
+initialGuess=numpy.array([1,1,1,1])
+fit2=minimize(quad,initialGuess,method="Nelder-Mead",options={'disp': True},args=data)
 
+from scipy import stats 
 
+teststat=2*(fit.fun-fit2.fun)
+df=len(fit2.x)-len(fit.x)
+1-stats.chi2.cdf(teststat,df)
 
-
-
-
-
-
-
-
-
-
+#linear is more apropriate because adding in that extra variable dosen't do anything 
 
 
 
@@ -120,23 +125,33 @@ import scipy
 import scipy.integrate as spint
 from plotnine import *
 
-def ddSim(y,t0,r1,r2,N1a11,N2a12,N2a22,N1a21): 
+def ddSim(y,t0,r1,r2,a11,a12,a22,a21): 
     N1=y[0] 
     N2=y[1]
-    dN1dt=r1*(1-N1a11-N2a12)*N1
-    dN2dt=r2*(1-N2a22-N1a21)*N2
+    dN1dt=r1*(1-N1*a11-N2*a12)*N1
+    dN2dt=r2*(1-N2*a22-N1*a21)*N2
     return [dN1dt,dN2dt]
-params=(0.5,100,#how do I know the other parameters? do I just make them up till they look good?)
+
 N0=[0.01,0.01]
 times=range(0,100)
-modelSim=spint.odent(func=ddSim,y0=N0,t=times,args=params)
-modelOutput=pandas.dataframe
-#I also don't get the last part of the queston? is that saying try out a few different alphas 
-#and see what one works the best? 
 
+params=(0.5,100,2,0.5,2,0.5)
+modelSim=spint.odeint(func=ddSim,y0=N0,t=times,args=params)
+modelOutput=pandas.DataFrame({"t":times,"N1":modelSim[:,0],"N2":modelSim[:,1]})
+a=ggplot(modelOutput,aes(x="t",y="N1"))+geom_line(color="red")+theme_classic()
+a+geom_line(modelOutput,aes(x="t", y="N2"),color="blue")
 
+params2=(0.5,100,2,2,2,2)
+modelSim=spint.odeint(func=ddSim,y0=N0,t=times,args=params2)
+modelOutput=pandas.DataFrame({"t":times,"N1":modelSim[:,0],"N2":modelSim[:,1]})
+b=ggplot(modelOutput,aes(x="t",y="N1"))+geom_line(color="red")+theme_classic()
+b+geom_line(modelOutput,aes(x="t", y="N2"),color="blue")
 
-
+params3=(0.5,100,1,.6,2,.3)
+modelSim=spint.odeint(func=ddSim,y0=N0,t=times,args=params3)
+modelOutput=pandas.DataFrame({"t":times,"N1":modelSim[:,0],"N2":modelSim[:,1]})
+c=ggplot(modelOutput,aes(x="t",y="N1"))+geom_line(color="red")+theme_classic()
+c+geom_line(modelOutput,aes(x="t", y="N2"),color="blue")
 
 
 
